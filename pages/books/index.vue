ad3a1f900fd5964a2641abc8864272169ba89ba5
ry-113 <yamado0326@gmail.com>
2023-11-23 11:11:05 +0900

    getAllBooksで取得したすべての本をv-forで表示

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <p>本の一覧ページです</p>
+    <h1 class="text-3xl mb-10">本の一覧ページです</h1>
+    <ul>
+        <li v-for="book in allBooks" :key="book.bookid" class="mb-3">
+            <p>{{ book.title }}</p>
+            <p>{{ book.author }}</p>
+        </li>
+    </ul>
 </template>
 <script setup lang="ts">
 const { getAllBooks } = useBookStore();
