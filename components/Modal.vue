b9bf013f49bfa659892d8792ed7041c52b3c6abe
ry-113 <yamado0326@gmail.com>
2023-11-29 23:06:30 +0900

    slotを使った柔軟なモーダルコンポーネントを用意

---
+++ components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- slotでボタンの名前と中身のHTMLを記述して、propsで一意のmodal-idを渡す -->
+  <button class="btn w-32" :onclick="`${props.modalId}.showModal()`">
+    <slot name="actionName">デフォルト</slot>
+  </button>
+  <dialog :id="`${props.modalId}`" class="modal">
+    <div class="modal-box">
+      <form method="dialog">
+        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+          ✕
+        </button>
+      </form>
+      <slot/>
+    </div>
+  </dialog>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  modalId: string;
+}
+
+const props = defineProps<Props>();
+</script>
--- pages/books/[bookid]-detail.vue
+++ pages/books/[bookid]-detail.vue
@@ -6,7 +6,9 @@
           <ul>
             <li><NuxtLink to="/books">本棚</NuxtLink></li>
             <li>
-              <NuxtLink :to="`/books/${book?.genre}`">{{ book?.genre }}</NuxtLink>
+              <NuxtLink :to="`/books/${book?.genre}`">{{
+                book?.genre
+              }}</NuxtLink>
             </li>
             <li>{{ book?.title }}</li>
           </ul>
@@ -22,14 +24,28 @@
             <p>ジャンル: {{ book?.genre }}</p>
             <ReviewDisplay :rating="rating" class="mt-3"></ReviewDisplay>
 
-            <div class="action-btns flex absolute bottom-4 right-0 gap-5">
-              <button class="btn w-32">レビュー投稿</button>
-              <button class="btn w-32">借りる</button>
-              <button class="btn w-32">返す</button>
+            <div class="action--btns flex absolute bottom-4 right-0 gap-5">
+              <Modal modal-id="review">
+                <template #actionName> レビュー投稿 </template>
+                <h1>レビュー投稿</h1>
+                <p>このモーダルはレビューを投稿できます。</p>
+              </Modal>
+              <Modal modal-id="borrow">
+                <template #actionName> 借りる </template>
+                <h1>貸出リクエスト</h1>
+                <p>このモーダルは貸出のリクエストができます。</p>
+              </Modal>
+              <Modal modal-id="returnBook">
+                <template #actionName> 返す </template>
+                <h1>返却リクエスト</h1>
+                <p>このモーダルは返却のリクエストができます。</p>
+              </Modal>
             </div>
           </div>
 
-          <div class="contents--container py-10 flex justify-between items-start">
+          <div
+            class="contents--container py-10 flex justify-between items-start"
+          >
             <img src="/img/book_dummy.png" alt="" class="w-[45%] xl:w-[35%]" />
             <div class="book--info w-[50%]">
               <ul class="flex gap-3 label--list flex-wrap">
@@ -51,28 +67,40 @@
               <div class="review--box">
                 <h2 class="text-xl mt-8 mb-2">レビュー</h2>
                 <p class="mb-8">クリックすると全文を表示できます。</p>
-                <Carousel :items-to-show="1" :wrap-around="true" :autoplay="5000" :transition="800">
+                <Carousel
+                  :items-to-show="1"
+                  :wrap-around="true"
+                  :autoplay="5000"
+                  :transition="800"
+                >
                   <Slide v-for="review in reviews" :key="review.reviewid">
-                    <div class="carousel__item bg-zinc-100 p-4 rounded-xl text-left w-[75%] h-[35vh]">
-                      <div class="review--header flex items-center justify-between mb-3">
+                    <div
+                      class="carousel__item bg-zinc-100 p-4 rounded-xl text-left w-[75%] h-[35vh]"
+                    >
+                      <div
+                        class="review--header flex items-center justify-between mb-3"
+                      >
                         <div class="reviewer-info flex items-center gap-3">
-                          <img :src="`${review.photo}`" alt="" class="rounded-full w-10" />
+                          <img
+                            :src="`${review.photo}`"
+                            alt=""
+                            class="rounded-full w-10"
+                          />
                           <p class="text-lg">{{ review.username }}</p>
                         </div>
                         <p>{{ review.timestamp }}</p>
                       </div>
 
-                      <ReviewDisplay :rating="review.rating" />
+                      <ReviewDisplay :rating="Number(review.rating)" />
 
                       <div class="review--box mt-6 text-overflow-lines">
                         <h3 class="mb-3 font-semibold">{{ review.title }}</h3>
                         <p>{{ review.description }}</p>
                       </div>
-                      
                     </div>
                   </Slide>
                   <template #addons>
-                    <Navigation/>
+                    <Navigation />
                   </template>
                 </Carousel>
               </div>
@@ -85,8 +113,8 @@
 </template>
 
 <script setup lang="ts">
-import 'vue3-carousel/dist/carousel.css';
-import type {Review} from "@/composables/useReviewStore"
+import "vue3-carousel/dist/carousel.css";
+import type { Review } from "@/composables/useReviewStore";
 definePageMeta({
   layout: false,
 });
@@ -98,28 +126,27 @@ const labels = computed(() => book?.labels);
 
 const getBgColor = (color: string) => {
   switch (color) {
-    case 'red':
-      return 'bg-red-400';
-    case 'blue':
-      return 'bg-blue-400';
-    case 'green':
-      return 'bg-green-400';
-    case 'yellow':
-      return 'bg-yellow-400';
-    case 'purple':
-      return 'bg-purple-400';
+    case "red":
+      return "bg-red-400";
+    case "blue":
+      return "bg-blue-400";
+    case "green":
+      return "bg-green-400";
+    case "yellow":
+      return "bg-yellow-400";
+    case "purple":
+      return "bg-purple-400";
   }
 };
 
 const { getReviews } = useReviewStore();
 const reviews: Review[] = await getReviews(book?.bookid);
 
-const rating = computed(():number => {
-  let total:number = 0;
-  reviews.forEach(review => total += Number(review.rating));
+const rating = computed((): number => {
+  let total: number = 0;
+  reviews.forEach((review) => (total += Number(review.rating)));
   const ratingAvr = total / reviews.length;
-  const rating = Math.round(ratingAvr * 10) / 10 //小数第2位を四捨五入
+  const rating = Math.round(ratingAvr * 10) / 10; //小数第2位を四捨五入
   return rating;
 });
-
 </script>
