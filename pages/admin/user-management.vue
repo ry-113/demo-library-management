f8db9f55ebd665a6794dacca1b36bfe3e279e3e8
RyoYamamoto-g <yamado0326@gmail.com>
2023-12-04 11:31:08 +0900

    ユーザー一覧をdaisyUIのtableコンポーネントで表示

--- pages/admin/user-management.vue
+++ pages/admin/user-management.vue
@@ -1,12 +1,47 @@
 <template>
-    <p>管理者のユーザー管理ページ</p>
-    <ul>
-        <li v-for="user in allUsers" :key="user.uid">
-            {{ user.role }}-{{ user.name }}
-        </li>
-    </ul>
+  <div v-if="isLoading" class="w-full h-[85vh] flex justify-center">
+    <span class="loading loading-spinner loading-lg"></span>
+  </div>
+  <table v-else class="table">
+    <thead>
+      <tr>
+        <td>ユーザーID</td>
+        <td>名前</td>
+        <td>メールアドレス</td>
+        <td>権限</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in allUsers" :key="user.uid">
+        <th>{{ user.uid }}</th>
+        <td>
+          <div class="flex items-center gap-3">
+            <div class="avatar">
+              <div class="mask mask-squircle w-12 h-12">
+                <img
+                  :src="`${user.photo}`"
+                />
+              </div>
+            </div>
+            <div>
+              <div class="font-bold">{{ user.name }}</div>
+            </div>
+          </div>
+        </td>
+        <td>
+            {{ user.email }}
+        </td>
+        <td>
+            {{ user.role }}
+        </td>
+        <td>
+         ◇
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup langg="ts">
-const {allUsers, isLoading, getAllUsers} = useUserStore();
+const { allUsers, isLoading, getAllUsers } = useUserStore();
 </script>
